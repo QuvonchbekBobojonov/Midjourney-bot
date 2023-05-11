f51f90139a71dcfbf2c8b71e8e75e03ccfade358
QuvonchbekBobojonov <111079758+QuvonchbekBobojonov@users.noreply.github.com>
2023-05-11 23:40:50 +0500

    Update create.py

--- create.py
+++ create.py
@@ -1,7 +1,8 @@
 import openai
+import requests
 import json
 from deep_translator import GoogleTranslator
-from config import OPENAI_TOKEN
+from config import OPENAI_TOKEN, API_TOKEN
 
 
 def image_create_openai(text):
@@ -13,4 +14,18 @@ def image_create_openai(text):
         size="256x256",
     )
     return response["data"][0]["url"]
-                                           
+
+def images_create(text):
+    text = GoogleTranslator(source='auto', target='en').translate(text)
+
+    r = requests.post('https://clipdrop-api.co/text-to-image/v1',
+      files = {
+          'prompt': (None, text, 'text/plain')
+      },
+      headers = { 'x-api-key': API_TOKEN}
+    )
+    return r.content
+
+
+if __name__ == "__main__":
+    print(images_create_relicate("quyon bilan bo'ri va "))                    
