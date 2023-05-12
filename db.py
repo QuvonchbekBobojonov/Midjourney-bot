94e9a9a90020d81d3392c574dcc2e4f2afe36b16
QuvonchbekBobojonov <111079758+QuvonchbekBobojonov@users.noreply.github.com>
2023-05-12 20:27:25 +0500

    Update db.py

--- db.py
+++ db.py
@@ -1,28 +1,48 @@
-import sqlite3
+import psycopg2
 
 
 class DataBase:
-    def __init__(self, path):
-        self.path = path
-        self.conn = sqlite3.connect(self.path)
+    def __init__(self, host, port, database, user, password):
+        self.host = host
+        self.port = port
+        self.database = database
+        self.user = user
+        self.password = password
+
+        self.conn = psycopg2.connect(host=self.host, port=self.port, database=self.database, user=self.user, password=self.password)
         self.cursor = self.conn.cursor()
 
-        self.cursor.execute('''CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, user_id TEXT,
+        self.cursor.execute('''CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY, user_id TEXT,
          active TEXT, long TEXT)''')
 
     def user_exists(self, user_id):
         with self.conn:
-            result = self.cursor.execute("SELECT * FROM `users` WHERE `user_id` = ?", (user_id,)).fetchmany(1)
-            return bool(len(result))
+            self.cursor.execute("SELECT * FROM users WHERE user_id = %s::text", (user_id,))
+            result = self.cursor.fetchall()
+            return len(result) > 0
+            
 
 
     def create_user(self, user_id):
         with self.conn:
-            self.cursor.execute(f"INSERT INTO users (user_id, active) VALUES ('{user_id}', '1')")
+            self.cursor.execute("INSERT INTO users (user_id, active) VALUES (%s, '1')", (user_id,))
             self.conn.commit()
+
+    def old_get_users(self):
+        with self.conn:
+            self.cursor.execute("SELECT * FROM users")
+            rows = self.cursor.fetchall()
+            return rows
 
     def get_users(self):
         with self.conn:
             self.cursor.execute("SELECT * FROM users")
             rows = self.cursor.fetchall()
             return rows
+
+    def get_users_active(self, user_id):
+        with self.conn:
+            result = self.cursor.execute("SELECT active FROM users WHERE user_id = %s", (user_id,))
+            return result[0][0]
+    
+    
