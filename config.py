089e7effc48d4e12805448cf994f2e97aef0cfa2
QuvonchbekBobojonov <111079758+QuvonchbekBobojonov@users.noreply.github.com>
2023-05-12 20:28:45 +0500

    Update config.py

--- config.py
+++ config.py
@@ -9,3 +9,11 @@ OPENAI_TOKEN = env.str("OPENAI_TOKEN")
 API_TOKEN = env.str("API_TOKEN")
 
 ADMIN_ID = env.str("ADMIN_ID")
+
+# DATABESE config
+
+HOST = env.str("HOST")
+PORT = env.str("PORT")
+DATABESE = env.str("DATABASE")
+USER = env.str("USER")
+PASSWORD = env.str("PASSWORD")
