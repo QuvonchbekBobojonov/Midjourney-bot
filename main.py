c92995fb2f7c379424290ed341bcf4dd72584668
QuvonchbekBobojonov <111079758+QuvonchbekBobojonov@users.noreply.github.com>
2023-05-14 09:09:40 +0500

    Update main.py

--- main.py
+++ main.py
@@ -41,22 +41,23 @@ async def send_admin(msg: types.Message):
 async def send_photo(message: types.Message):
     user_id = message.from_user.id
     username = message.from_user.full_name
+    await message.answer("Bot yangilanmoda \n iltimos keyinroq qayta urinib ko'ring.")
     if not db.user_exists(user_id):
         db.create_user(user_id)
         await bot.send_message(ADMIN_ID, "🆕 Yangi Foydalanuvchi! \n"
                                          f"Umumiy: [{len(db.get_users())}] \n"
                                          f"Ismi: {username}")
-    passbar = await bot.send_message(message.from_user.id, text=10*'⬜'+'0%')
-    img = images_create(message.text)
-    for i in range(1, 11):
-        green = i*'🟩'
-        white = (10-i) * '⬜'
-        await bot.send_chat_action(message.from_user.id, types.ChatActions.UPLOAD_PHOTO)
-        await passbar.edit_text(f"{green}{white} {i*10}%")
-    await passbar.delete()
-    await message.answer_photo(img,
-                               caption='Sizning rasmigiz.\n \nAgar rasm siz hohlaganday'
-                                       ' bo\'lmasa matn Ingliz tilida yozing.')
+    # passbar = await bot.send_message(message.from_user.id, text=10*'⬜'+'0%')
+    # img = images_create(message.text)
+    # for i in range(1, 11):
+    #    green = i*'🟩'
+    #    white = (10-i) * '⬜'
+    #   await bot.send_chat_action(message.from_user.id, types.ChatActions.UPLOAD_PHOTO)
+    #   await passbar.edit_text(f"{green}{white} {i*10}%")
+    #await passbar.delete()
+    #await message.answer_photo(img,
+    #                           caption='Sizning rasmigiz.\n \nAgar rasm siz hohlaganday'
+    #                                   ' bo\'lmasa matn Ingliz tilida yozing.')
 
 
 @dp.message_handler(content_types=types.ContentTypes.ANY)
